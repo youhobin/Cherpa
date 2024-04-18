dc4d333b42d975be55e9064682b9de19701b2005
ghdb132 <ghdb132@naver.com>
2024-04-18 10:04:29 +0900

    init project

---
+++ src/main/java/com/cerpha/cerphaproject/CerphaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cerpha.cerphaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerphaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CerphaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cerpha/cerphaproject/CerphaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerpha.cerphaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerphaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
