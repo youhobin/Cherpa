b0a3dfb268d4a2067a8d2f1e4cc808b91c4b9dca
ghdb132 <ghdb132@naver.com>
2024-04-19 10:53:32 +0900

    feat Controller Validation ExceptionHandler

--- src/main/java/com/cerpha/cerphaproject/common/exception/ExceptionResponse.java
+++ src/main/java/com/cerpha/cerphaproject/common/exception/ExceptionResponse.java
@@ -2,16 +2,24 @@ package com.cerpha.cerphaproject.common.exception;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class ExceptionResponse {
 
     private int status;
     private String code;
     private String message;
+    private List<InvalidError> errorFieldList;
 
     public ExceptionResponse(ExceptionCode exceptionCode) {
         this.status = exceptionCode.getStatus();
         this.code = exceptionCode.getCode();
         this.message = exceptionCode.getMessage();
+    }
+
+    public ExceptionResponse(ExceptionCode exceptionCode, List<InvalidError> errorFieldList) {
+        this(exceptionCode);
+        this.errorFieldList = errorFieldList;
     }
 }
