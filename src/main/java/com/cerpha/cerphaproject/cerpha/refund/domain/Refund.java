171b67c9ce08841e960d9b74dfe2cad743e18b9f
ghdb132 <ghdb132@naver.com>
2024-04-20 22:02:07 +0900

    feat Refund Entity #28

---
+++ src/main/java/com/cerpha/cerphaproject/cerpha/refund/domain/Refund.java
@@ -0,0 +1,26 @@
+package com.cerpha.cerphaproject.cerpha.refund.domain;
+
+import com.cerpha.cerphaproject.cerpha.BaseTimeEntity;
+import com.cerpha.cerphaproject.cerpha.order.domain.Order;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Refund extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refund_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private String reason;
+
+}
