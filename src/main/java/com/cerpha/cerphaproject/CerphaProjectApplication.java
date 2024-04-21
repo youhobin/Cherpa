a9f690edeb25970c61486efbad37d8c9a2ac4431
ghdb132 <ghdb132@naver.com>
2024-04-21 14:15:00 +0900

    feat 주문 상태 변경 시 Scheduler 사용 #31

--- src/main/java/com/cerpha/cerphaproject/CerphaProjectApplication.java
+++ src/main/java/com/cerpha/cerphaproject/CerphaProjectApplication.java
@@ -3,9 +3,11 @@ package com.cerpha.cerphaproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class CerphaProjectApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cerpha/cerphaproject/cerpha/order/repository/OrderRepository.java
+++ src/main/java/com/cerpha/cerphaproject/cerpha/order/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package com.cerpha.cerphaproject.cerpha.order.repository;
 
 import com.cerpha.cerphaproject.cerpha.order.domain.Order;
+import com.cerpha.cerphaproject.cerpha.order.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT o FROM Order o WHERE o.status = :orderStatus AND o.updatedAt BETWEEN :start AND :end")
+    List<Order> findOrdersByStatusAndUpdatedAtBetween(OrderStatus orderStatus, LocalDateTime start, LocalDateTime end);
 }
